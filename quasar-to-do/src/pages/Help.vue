dd119f1af10d3dbfd35293e55f8faff25e2f32d3
sebaschen <sphilip1206@gmail.com>
2020-06-10 22:00:49 +0800

    quasar dev init

---
+++ quasar-to-do/src/pages/Help.vue
@@ -0,0 +1,12 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none"> Help </h5>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel id consequatur aut enim qui! Numquam obcaecati vitae, molestiae nobis animi maiores eaque quo aut recusandae deserunt libero id atque reiciendis!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel id consequatur aut enim qui! Numquam obcaecati vitae, molestiae nobis animi maiores eaque quo aut recusandae deserunt libero id atque reiciendis!</p>
+  </q-page>
+</template>
+
+<script>
+export default {
+}
+</script>
---
+++ quasar-to-do/src/pages/Todo.vue
@@ -0,0 +1,101 @@
+<template>
+  <q-page class="bg-grey-3 column">
+    <div class="row q-pa-sm bg-primary">
+      Add Task
+    </div>
+      <q-list 
+      class="bg-white"
+      separator
+      bordered>
+      
+
+      <q-item  
+      v-for="(task,index) in tasks"
+      :key="task.title"
+      @click="task.done=!task.done"
+      :class="{ 'done bg-blue-1' :task.done}"
+      clickable
+      v-ripple>
+        <q-item-section avatar>
+          <q-checkbox 
+          v-model="task.done"
+          class="no-pointer-events"          
+          color="primary" />
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>{{task.title}}</q-item-label>
+        </q-item-section>
+        <q-item-section
+        v-if="task.done"
+        side>          
+          <q-btn
+          @click.stop="deleteTask(index)" 
+          flat 
+          round
+          dense 
+          color="primary" 
+          icon="delete" />
+
+        </q-item-section>
+      </q-item>
+
+      
+      </q-item>
+    </q-list>
+  </q-page>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      tasks:[
+        {
+          title:'Get Apples',
+          done: false
+        },
+        {
+          title:'Eat Apples',
+          done: true
+        },
+        {
+          title:'Poo Apples',
+          done: false
+        },
+
+      ]
+
+    }
+  },
+  methods:{
+    deleteTask(index){
+
+    
+      this.$q.dialog({
+        title: 'Confirm',
+        message: 'Please confirm if you really wanna delete this item',
+        cancel: true,
+        persistent: true
+      }).onOk(() => {
+        this.tasks.splice(index,1)
+        this.$q.notify({
+        message: 'Task Delete Correctly',
+        color: 'tortoise'
+      })
+      })
+      
+    }
+    }
+
+  }
+
+</script>
+
+<style lang="scss">
+  .done {
+    .q-item__label{
+      text-decoration: line-through;
+      color: #bbbbbb;
+    }
+  }
+</style>
